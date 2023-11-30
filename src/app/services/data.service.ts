5c3851ae49e5b0f5761e4666ce780982e9300bb1
Mark Thompson <mthompson@bitovi.com>
2023-11-29 23:18:10 -0800

    added finnish notation

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,17 +11,17 @@ export class DataService {
     this.countSignal.update(prev => prev + 1);
   }
 
-  private readonly countSubject = new BehaviorSubject(0);
-  readonly countObservable = this.countSubject.asObservable();
+  private readonly countSubject$ = new BehaviorSubject(0);
+  readonly countObservable$ = this.countSubject$.asObservable();
 
   incObservable(): void {
-    this.countSubject.next(this.countSubject.value + 1);
+    this.countSubject$.next(this.countSubject$.value + 1);
   }
 
-  private readonly countSubject2 = new BehaviorSubject(0);
-  readonly countObservable2 = this.countSubject2.asObservable();
+  private readonly countSubject2$ = new BehaviorSubject(0);
+  readonly countObservable2$ = this.countSubject2$.asObservable();
 
   incObservable2(): void {
-    this.countSubject2.next(this.countSubject2.value + 1);
+    this.countSubject2$.next(this.countSubject2$.value + 1);
   }
 }
